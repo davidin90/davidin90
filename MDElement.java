d950d545fa9dd5e34d856e58e734f0a7b892d310
poong44 <dh3115@naver.com>
2016-11-25 19:05:23 +0900

    Add files via upload

---
+++ MDElement.java
@@ -0,0 +1,8 @@
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public interface MDElement{
+	public ArrayList<Node> node=new ArrayList<Node>();
+	public void accept(MDElementVisitor v) throws IOException;
+}
