9296f29277a7cbdc4f6e2df382ee638821e569ce
kimdh2018 <kimdh2014@naver.com>
2016-11-25 15:02:35 +0900

    Add files via upload

--- Plain.java
+++ Plain.java
@@ -1,3 +1,4 @@
+
 import org.w3c.tidy.*;
 
 import java.io.ByteArrayInputStream;
@@ -10,71 +11,71 @@ import java.util.ArrayList;
 
 
 public class Plain implements MDElementVisitor{
-	private ArrayList<Node> node = new ArrayList<Node>();
-	private String s;
-	public Plain(){
-	}
+   private ArrayList<Node> node = new ArrayList<Node>();
+   private String s;
+   public Plain(){
+   }
 
-	public void visitDocument(Document document) {
-		// TODO Auto-generated method stub
+   public void visitDocument(Document document) {
+      // TODO Auto-generated method stub
 
-	}
-	public void visitNode(Node n){
-		node.add(n);
+   }
+   public void visitNode(Node n){
+      node.add(n);
 
-	}
-	public void visitToken(Token token){
+   }
+   public void visitToken(Token token){
 
-	}
-	public void makehtml(String string) throws IOException{
+   }
+   public void makehtml(String string) throws IOException{
 
-		try{
-
-
-			File html0=new File(string);
-			 if(html0.isFile()){
-	             System.out.println("That file has already existed.");
-	         }
-			 else{
-				    s="<!Doctype html PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\n"+"<html>\n"+"<head>\n"+"<title></title>\n"
-				   +"</head>\n"+"<body>\n";
-		            for(int i=0; i<node.size(); i++){
-		            	s = s+(node.get(i)).strings+"\n";
-		            	System.out.println((node.get(i)).strings);
-		            }
-		            s= s+"</body>\n"+"</html>\n";
-	                if(isValid(s)){
-	                	FileWriter html=new FileWriter(string);
-	                	html.write(s);
-
-			            html.close();
-			            System.out.println("Html file is created.");
-			            node.clear();
-
-	                }
-	                else
-	                	System.out.println("Wrong syntax");
+      try{
 
 
+         File html0=new File(string);
+          if(html0.isFile()){
+                System.out.println("That file has already existed.");
+                node.clear();
+            }
+          else{
+                s="<!Doctype html PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\n"+"<html>\n"+"<head>\n"+"<title></title>\n"
+               +"</head>\n"+"<body>\n";
+                  for(int i=0; i<node.size(); i++){
+                     s = s+(node.get(i)).strings+"\n";
+                  }
+                  s= s+"</body>\n"+"</html>\n";
+                   if(isValid(s)){
+                      FileWriter html=new FileWriter(string);
+                      html.write(s);
 
-			 }
-		}
+                     html.close();
+                     System.out.println("Html file is created.");
+                     node.clear();
 
-		catch(IOException e){
-			e.getMessage();
-		}
+                   }
+                   else
+                      System.out.println("Wrong syntax");
 
-	}
 
-	public void visitNode(ArrayList<Node> node) {
-		// TODO Auto-generated method stub
 
-	}
-	private boolean isValid(String htmlData){
-			   Tidy tidy = new Tidy();
-			   InputStream stream = new ByteArrayInputStream(htmlData.getBytes());
-			   tidy.parse(stream, System.out);
-			   return (tidy.getParseErrors() == 0);
-	}
+          }
+      }
+
+      catch(IOException e){
+         e.getMessage();
+      }
+
+   }
+
+   public void visitNode(ArrayList<Node> node) {
+      // TODO Auto-generated method stub
+
+   }
+   private boolean isValid(String htmlData){
+            Tidy tidy = new Tidy();
+            InputStream stream = new ByteArrayInputStream(htmlData.getBytes());
+            tidy.parse(stream, System.out);
+            return (tidy.getParseErrors() == 0);
+   }
 
 }
